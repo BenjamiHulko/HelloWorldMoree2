038dc7bbfc917e750382a4616a8182482c12f5da
ben <vensan90@gmail.com>
2023-05-20 11:29:10 +0300

    n3

--- HelloWorldMoree2/ViewController.swift
+++ HelloWorldMoree2/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var HelloWorldLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        HelloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
 
+    }
+    
+    @IBAction func makeButtonAction(_ sender: Any) {
+        
+        if HelloWorldLabel.isHidden {
+            HelloWorldLabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            HelloWorldLabel.isHidden = true
+            toggleButton.setTitle("Show Text", for: .normal)
+        }
+        
+        
+        //HelloWorldLabel.isHidden = false
+    }
+    
 
 }
 
